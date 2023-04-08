1bddc33afa114450e6bf609896b381e09ecc4752
Alif Arfab Pavel <aap.pavel@gmail.com>
2023-04-08 22:52:26 +0600

    added problem description

--- 70-climbing_stairs.py
+++ 70-climbing_stairs.py
@@ -1,3 +1,46 @@
+# Climbing Stairs
+#
+# [Easy] [AC:52.2% 2.3M of 4.4M] [filetype:python3]
+#
+# You are climbing a staircase. It takes n steps to reach the
+# top.
+#
+# Each time you can either climb 1 or 2 steps. In how many
+# distinct ways can you climb to the top?
+#
+# Example 1:
+#
+# Input: n = 2
+#
+# Output: 2
+#
+# Explanation: There are two ways to climb to the top.
+#
+# 1. 1 step + 1 step
+#
+# 2. 2 steps
+#
+# Example 2:
+#
+# Input: n = 3
+#
+# Output: 3
+#
+# Explanation: There are three ways to climb to the top.
+#
+# 1. 1 step + 1 step + 1 step
+#
+# 2. 1 step + 2 steps
+#
+# 3. 2 steps + 1 step
+#
+# Constraints:
+#
+# 1 <= n <= 45
+#
+# [End of Description]:
+
+
 class Solution:
     def factorial(
         self,
