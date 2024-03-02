66f71a7ef2385620513c868948354d7e611c08e2
Alif Arfab Pavel <aap.pavel@gmail.com>
2024-03-02 16:13:26 +0600

    chore: formatted code

--- 67.add-binary.py
+++ 67.add-binary.py
@@ -14,25 +14,26 @@
 # Testcase Example:  '"11"\n"1"'
 #
 # Given two binary strings a and b, return their sum as a binary string.
-# 
-# 
+#
+#
 # Example 1:
 # Input: a = "11", b = "1"
 # Output: "100"
 # Example 2:
 # Input: a = "1010", b = "1011"
 # Output: "10101"
-# 
-# 
+#
+#
 # Constraints:
-# 
-# 
+#
+#
 # 1 <= a.length, b.length <= 10^4
 # a and b consist only of '0' or '1' characters.
 # Each string does not contain leading zeros except for the zero itself.
-# 
-# 
 #
+#
+#
+
 
 # @lc code=start
 class Solution:
@@ -54,7 +55,7 @@ class Solution:
         a = "0" * (i - lenA) + a
         b = "0" * (i - lenB) + b
         i -= 1
-        
+
         while i >= 0:
             b1 = a[i]
             b2 = b[i]
@@ -62,16 +63,11 @@ class Solution:
             print(add)
             tempS = self.bitAdd(add[1], c)
             s = tempS[1] + s
-            c = "1" if add[0] == "1" or tempS[0] == "1" else "0" 
+            c = "1" if add[0] == "1" or tempS[0] == "1" else "0"
             i -= 1
         if c == "1":
             s = c + s
         return s
-                    
-                    
-                    
-                    
-            
-        
-# @lc code=end
 
+
+# @lc code=end
